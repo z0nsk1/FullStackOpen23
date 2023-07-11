b03bde084c00bff6d58385e0b9f3ba0d47ab8b91
z0nsk1 <nurmjo@gmail.com>
2023-07-11 13:31:26 +0300

    osa1 tehtävät 1.7-1.9, if ratkaisu hieman kyseenalainen?

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -10,27 +10,54 @@ const Header = ({ props }) => (
   <h1>{props}</h1>
 )
 
+const Statistics = ({text, value}) => {
+  if (text === "positive") {
+    return (
+      <p>{text} {value} %</p>
+    )
+  }
+  return (
+    <p>{text} {value}</p>
+  )
+}
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
 
   const handleGood = () => {
     console.log('Adding good feedback... value before', good)
     setGood(good + 1)
+    setAll(all + 1)
   }
 
   const handleNeutral = () => {
     console.log('Adding neutral feedback... value before', neutral)
     setNeutral(neutral + 1)
+    setAll(all + 1)
   }
 
   const handleBad = () => {
     console.log('Adding bad feedback... value before', bad)
     setBad(bad + 1)
+    setAll(all + 1)
   }
 
+  if (all === 0) {
+    return (
+      <div>
+        <Header props={'give feedback'}/>
+        <Button handleClick={handleGood} text='good' />
+        <Button handleClick={handleNeutral} text='neutral' />
+        <Button handleClick={handleBad} text='bad' />
+        <Header props={'statistics'}/>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
   return (
     <div>
       <Header props={'give feedback'}/>
@@ -39,9 +66,12 @@ const App = () => {
       <Button handleClick={handleBad} text='bad' />
       <Header props={'statistics'}/>
       <div style={{lineHeight:'5px'}}>
-        <p style={{color:'darkgreen'}}>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p style={{color:'red'}}>bad {bad}</p>
+        <Statistics text={'good'} value={good}/>
+        <Statistics text={'neutral'} value={neutral}/>
+        <Statistics text={'bad'} value={bad}/>
+        <Statistics text={'all'} value={all}/>
+        <Statistics text={'average'} value={(good - bad) / all}/>
+        <Statistics text={'positive'} value={(good / all) * 100}/>
       </div>
     </div>
   )
