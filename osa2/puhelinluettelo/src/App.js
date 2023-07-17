007f5c5a37de007f81ccc6d5cc030fcafc8ed2f2
z0nsk1 <nurmjo@gmail.com>
2023-07-17 19:01:58 +0300

    tehtävät 2.12-2.15

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
+import personService from './communication/persons'
 
 const Filter = ({filter, handleFilter}) => (
   <form>
@@ -7,13 +7,19 @@ const Filter = ({filter, handleFilter}) => (
   </form>
 )
 
-//  Komponentti, joka filtteröi näytettävät henkilöt
-const Persons = ({persons, filter}) => {
-  const personsToShow = persons.filter(person => person.name.toLowerCase().startsWith(filter.toLowerCase()))
+const Persons = ({person, removePerson}) => {
   return (
-    <div>
-     {personsToShow.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
-    </div>
+    <p> 
+      {person.name} {person.number}
+      <button onClick={() => {
+        if (window.confirm(`Delete ${person.name}?`)) {
+          //window.alert(`OK, deleting ${person.name}!`)
+          removePerson(person.id)
+        }
+      }}>
+        delete
+      </button>
+    </p>
 )}
 
 // Henkilön verkkosivulle lisäävä komponentti
@@ -27,23 +33,54 @@ const AddPerson = ({addPerson, newName, newNumber, handleNewName, handleNewNumbe
 
 const App = () => {
   const [persons, setPersons] = useState([])
-  const [newName, setNewName] = useState('Lisää uusi yhteystieto')
-  const [newNumber, setNewNumber] = useState('Lisää numero')
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
 
-  // Efekti
-  const hook = () => {
-    console.log('Effect')
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        console.log('Promise fulfilled')
-        setPersons(response.data)
-      })
-  }
+useEffect (() => {
+  personService
+    .getAll()
+    .then(initialPersons => {
+      setPersons(initialPersons)
+    })
+  }, [])
+
+const removePerson = (id) => {
+  const person = persons.find(p => p.id === id)
+  const removedPerson = {...person}
+  console.log('Poistettavan henkilön id: ', removedPerson.id)
   
-  console.log('Rendering', persons.length, 'persons')
-  useEffect(hook, [])
+  personService
+    .remove(removedPerson.id)
+    .then(returnedPerson => {
+      setPersons(persons.filter(p => p.id !== id))
+    })
+    .catch(error => {
+      alert(
+        `Error, deleting failed`
+      )
+      setPersons(persons.filter(p => p.id !== id))
+    })
+}
+
+const updateNumber = (newPerson) => {
+  const person = persons.find(p => p.name === newPerson.name)
+  console.log(person)
+  const changedPerson = {...person, number: newPerson.number}
+  console.log(changedPerson)
+
+  personService
+    .updateNumber(person.id, changedPerson)
+    .then(returnedPerson => {
+      setPersons(persons.map(p => p.id !== person.id ? p : returnedPerson))
+    })
+    .catch(error => {
+      alert(
+        `Error, updating failed`
+      )
+      setPersons(persons.filter(p => p.id !== person.id))
+    })
+}
 
 const addPerson = (event) => {
   event.preventDefault() // estetään lomakkeen lähetys
@@ -51,34 +88,44 @@ const addPerson = (event) => {
   const newPerson = { //Henkilö-olio
     name: newName,
     number: newNumber,
-    id: persons.length +1,
   }
   // Tutkitaan, onko henkilö jo lisätty ja annetaan varoitus, sekä poistutaan ennen henkilön lisäystä
   if (persons.find(person => person.name === newPerson.name)) {
-    return (
-      alert(`${newName} is already added to phonebook`)
-    )
+    if (window.confirm(`${newPerson.name} is already added to phonebook, replace the old number with a new one?`)) {
+      console.log(newPerson)
+      updateNumber(newPerson)
+    } else {
+      return (
+        console.log('Number update was cancelled')
+    )}
+  } else {
+      personService
+      .create(newPerson)
+      .then(returnedPerson => {
+        console.log(returnedPerson)
+        setPersons(persons.concat(returnedPerson))
+        console.log('New person added:', returnedPerson.name)
+        setNewName('')
+        setNewNumber('')
+    })
   }
-  setPersons(persons.concat(newPerson))
-  console.log('New person added:', newPerson.name)
-  setNewName('')
-  setNewNumber('')
 }
+
+// Näytettävät henkilöt
+const personsToShow = persons
+.filter(person => person.name.toLowerCase().startsWith(filter.toLowerCase()))
 
 // Nimikentän tapahtumankäsittelijä
 const handleNewName = (event) => {
-  console.log(event.target.value)
   setNewName(event.target.value)
 }
 
 // Puhelinkentän tapahtumankäsittelijä
 const handleNewNumber = (event) => {
-  console.log(event.target.value)
   setNewNumber(event.target.value)
 }
 
 const handleFilter = (event) => {
-  console.log(event.target.value)
   setFilter(event.target.value)
 }
 
@@ -89,7 +136,11 @@ const handleFilter = (event) => {
       <h2>Add a new contact</h2>
       <AddPerson addPerson={addPerson} newName={newName} newNumber={newNumber} handleNewName={handleNewName} handleNewNumber={handleNewNumber}/>
       <h2>Numbers</h2>
-      <Persons persons={persons} filter={filter}/>
+      <div>
+          {personsToShow.map(person =>
+            <Persons key={person.id} person={person} removePerson={() => removePerson(person.id)}/>
+          )}
+      </div>
     </div>
   )
 
