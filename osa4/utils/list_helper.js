0476547d1e9d46c1f9cf5a5d24a2251901c94bee
z0nsk1 <nurmjo@gmail.com>
2023-10-20 18:45:41 +0300

    tehtävä 4.4

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -1,7 +1,14 @@
-const dummy = (blogs) => {
-    return 1
+const totalLikes = (blogs) => {
+    if (blogs.length === 0) {
+        return 0
+    } else {
+        const likes = blogs.reduce((acc, blog) => {
+            return acc + blog.likes
+        }, 0)
+        return likes
+    }
 }
 
 module.exports = {
-    dummy
+    totalLikes
 }
