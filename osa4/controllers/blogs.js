026485dd063a783029b717a5c390c18394c0a5b4
z0nsk1 <nurmjo@gmail.com>
2023-10-24 17:30:32 +0300

    tehtävät 4.13 ja 4.14, testejä voisi vielä parannella ja refaktoroida, mutta ovat tehtävänantojen mukaiset

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -2,7 +2,7 @@
 
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
-const { info, errorInfo } = require('../utils/logger')
+const logger = require('../utils/logger')
 
 // Olemassa olevien blogien hakeminen
 blogsRouter.get('/', (request, response) => {
@@ -14,15 +14,14 @@ blogsRouter.get('/', (request, response) => {
 // Uuden blogin lisäys
 blogsRouter.post('/', (request, response, next) => {
   const body = request.body
-  info(request.body)
-
-  info(body.likes)
+  logger.info(request.body)
+  logger.info(body.likes)
 
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes === "" ? 0 : body.likes,
+    likes: body.likes === "" ? 0 : body.likes, // Jos likes-kentän arvo on "", laitetaan arvoksi 0, muuten likes-kentälle asetettu arvo
   })
 
   blog
@@ -32,6 +31,25 @@ blogsRouter.post('/', (request, response, next) => {
     }).catch(error => {
         next(error)
     })
+})
+
+blogsRouter.delete('/:id', async (req, res) => {
+  await Blog.findByIdAndRemove(req.params.id)
+  res.status(204).end()
+})
+
+blogsRouter.put('/:id', async (req, res) => {
+  const body = req.body
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  }
+
+  const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, blog, { new: true })
+  res.status(200).json(updatedBlog)
 })
 
 module.exports = blogsRouter
--- osa4/tests/blogs_api.test.js
+++ osa4/tests/blogs_api.test.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const helper = require('./test_helper')
 const app = require('../app') // Testit käyttävät ainoastaan app.js määriteltyä express-sovellusta
 
 const api = supertest(app)
@@ -26,6 +27,12 @@ test('blogs are identified with id field', async () => {
 })
 
 test('a new blog can be added', async () => {
+  const testBlog = {
+    title: "Sample Blog",
+    author: "admin",
+    url: "https://localhost:3001/api/blogs",
+    likes: 123
+  }
   await api.post('/api/blogs').send(testBlog)
   const res = await api.get('/api/blogs')
   expect(res.body).toHaveLength(4) // Määrän pitäisi kasvaa yhdellä
@@ -35,7 +42,7 @@ test('a new blog can be added', async () => {
   })
 })
 
-test("if blog doesn't have title or url fields, response is statuscode 400 and blog is not added", async() => {
+test("if blog doesn't have title or url fields, response is status code 400 and blog is not added", async() => {
   const testBlog1 = {
     author: "testiTimo",
     likes: 20
@@ -59,6 +66,29 @@ test('if likes are not given, its value is set to 0', async () => {
   .send(testBlog)
   const res = await api.get('/api/blogs')
   expect(res.body[4].likes).toBe(0)
+})
+
+test('a blog can be removed, succeeds with status code 204', async() => {
+  const blogs = await helper.blogsInDb()
+  const blogToDelete = blogs[3]
+  await api
+  .delete(`/api/blogs/${blogToDelete.id}`)
+  .expect(204)
+})
+
+test('a blog can be edited, succeeds with status code 200', async() => {
+  const blogs = await helper.blogsInDb()
+  const blogToEdit = blogs[blogs.length-1]
+  const editedBlog = {
+    title: "Sampleee",
+    author: "admin",
+    url: "http://localhost:3001/api/blogs",
+    likes: 2345
+  }
+  await api
+  .put(`/api/blogs/${blogToEdit.id}`)
+  .send(editedBlog)
+  .expect(200)
 })
 
 afterAll(async () => {
