0a22caafa3791361b670399f008069a2149cdc41
z0nsk1 <nurmjo@gmail.com>
2023-11-09 23:54:01 +0200

    tehtävät 4.6 ja 4.7, koko neljäs osio on nyt tehty

--- osa4/tests/list_helper.js
+++ osa4/tests/list_helper.js
@@ -1,5 +1,6 @@
 const Blog = require('../models/blog')
 //const User = require('../models/user')
+const Lodash = require('lodash')
 
 const totalLikes = (blogs) => {
   if (blogs.length === 0) {
@@ -25,8 +26,36 @@ const blogsInDb = async () => {
   return blogs.map(b => b.toJSON())
 }
 
+const mostBlogs = (blogs) => {
+  const blogsPerBlogger = Lodash.countBy(blogs.map(blog => blog.author)) // Mapataan ensin blogien omistajat taulukkoon, josta lodash.countby:n avulla selvitetään, kuinka monta kertaa henkilö esiintyy taulukossa
+  const max = Math.max(...Object.values(blogsPerBlogger)) // Etsitään suurin arvo äsken luodusta taulukosta
+  for (let blogger in blogsPerBlogger) { // Käydään taulukko läpi silmukalla
+    if (blogsPerBlogger[blogger] === max) { // Kun taulukosta löydetään suurimman arvon omaava alkio...
+      return { "author": blogger, "blogs": max }  // palautetaan se halutussa muodossa, eli author ja blogien määrä
+    }
+  }
+}
+
+const mostLikes = (blogs) => {
+  let likesPerBlogger = blogs.reduce((newObject, blog) => { // Käydään taulukko läpi reducella, newObject tulee olemaan uusi olio, johon arvoja laitetaan ja blog nykyisessä indeksissä oleva olio blogs-taulukossa
+    if (!Object.prototype.hasOwnProperty.call(newObject, blog.author)) { // Jos uudessa oliossa newObject ei vielä ole nykyisessä blog-oliossa olevaa authoria, alustetaan se arvolla nolla
+      newObject[blog.author] = 0
+    }
+    newObject[blog.author] += blog.likes // lisätään newObject-olioon nykyisen blog-olion authorille nykyisen blog-olion liket
+    return newObject // lopuksi palautetaan uusi olio
+  }, {}) // tyhjä olio on newObject:n alkuarvo, joka tarvitaan newObject:n alustukseen
+  const max = Math.max(...Object.values(likesPerBlogger)) // Haetaan luodusta oliosta suurin likejen arvo
+  for (let blogger in likesPerBlogger) { // Käydään taulukko läpi silmukalla
+    if (likesPerBlogger[blogger] === max) { // Kun taulukosta löydetään alkio, jolla on suurin like-määrä
+      return { "author": blogger, "likes": max } // Palautetaan kyseinen alkio halutussa muodossa
+    }
+  }
+}
+
 module.exports = {
   totalLikes,
   favoriteBlog,
-  blogsInDb
+  blogsInDb,
+  mostBlogs,
+  mostLikes
 }
